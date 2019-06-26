2a2dbc2b167a22c1f47382924d0caf354b03e4fe
Hannes Preishuber <hannesp@ppedv.de>
2019-06-26 13:58:30 +0200

    Page Handler /Route und Querystring

---
+++ aspnetcorewien/Pages/modul03/PgHandler.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcorewien.Pages.modul03
+{
+    public class PgHandlerModel : PageModel
+    {
+        public string Einstieg { get; set; }
+        public void OnGet()
+        {
+            Einstieg = "OnGet";
+        }
+        public void OnGetDemo()
+        {
+            Einstieg = "Page Handler";
+        }
+    }
+}
