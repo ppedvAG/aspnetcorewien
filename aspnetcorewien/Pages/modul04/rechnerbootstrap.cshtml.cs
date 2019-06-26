e8d2d6031c3cac631394df141c4b2fa8b27344cf
Hannes Preishuber <hannesp@ppedv.de>
2019-06-26 15:53:17 +0200

    Boostrap form sample Rechner

---
+++ aspnetcorewien/Pages/modul04/rechnerbootstrap.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcorewien.Pages.modul04
+{
+    public class rechnerbootstrapModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
