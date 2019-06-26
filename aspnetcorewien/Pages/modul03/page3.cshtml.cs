1aed850aea90a315d3c4557ae7fab7eaeaf84c35
Hannes Preishuber <hannesp@ppedv.de>
2019-06-26 12:20:32 +0200

    Razor Basics Rotue und Parameter

---
+++ aspnetcorewien/Pages/modul03/page3.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcorewien.Pages.modul03
+{
+    public class page3Model : PageModel
+    {
+        public int _Id { get; set; }
+        public void OnGet([FromQuery] int id)
+        {
+            _Id = id;
+        }
+    }
+}
