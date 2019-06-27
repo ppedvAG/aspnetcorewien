53dbc4864bea8660317aa315932490deff9bf776
Hannes Preishuber <hannesp@ppedv.de>
2019-06-27 10:34:07 +0200

    set Cookie und lese

---
+++ aspnetcorewien/Pages/Modul05/CookieTest.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcorewien.Pages.Modul05
+{
+    public class CookieTestModel : PageModel
+    {
+        public void OnGet()
+        {
+
+            
+            var options = new CookieOptions {
+                Domain = ".ppedv.de",
+                Expires = DateTime.Now.AddDays(30)
+                
+            };
+            Response.Cookies.Append("Hannes", "Wert",options);
+
+            var o = Request.Cookies["hannes"];
+
+        }
+    }
+}
