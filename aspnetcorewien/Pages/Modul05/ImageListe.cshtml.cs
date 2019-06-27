0c726a600957fe13168f87373725966c7c0d3e2d
Hannes Preishuber <hannesp@ppedv.de>
2019-06-27 14:20:01 +0200

    Thumbnail Liste

--- aspnetcorewien/Pages/Modul05/ImageListe.cshtml.cs
+++ aspnetcorewien/Pages/Modul05/ImageListe.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,16 @@ namespace aspnetcorewien.Pages.Modul05
 {
     public class ImageListeModel : PageModel
     {
+        public string[] dateien { get; set; }
         public void OnGet()
         {
+            var pfad = AppDomain.CurrentDomain.GetData("wwwroot").ToString();
+            dateien = Directory.GetFiles(pfad);
 
+        }
+        public string ClearPath(string datei)
+        {
+            return datei.Replace(AppDomain.CurrentDomain.GetData("wwwroot").ToString(), "");
         }
     }
 }
