e2cfb676019c4eba98f79d84975afadacc436398
Hannes Preishuber <hannesp@ppedv.de>
2019-06-27 11:36:08 +0200

    Cache Taghelper

---
+++ aspnetcorewien/Pages/Modul05/cache2.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcorewien.Pages.Modul05
+{
+    public class cache2Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
