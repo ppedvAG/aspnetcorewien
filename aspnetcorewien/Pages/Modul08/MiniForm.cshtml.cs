beaa5c483cbc376677497d3271e63ffafcfc3d43
Hannes Preishuber <hannesp@ppedv.de>
2019-06-28 11:16:13 +0200

    Form Validierung

---
+++ aspnetcorewien/Pages/Modul08/MiniForm.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcorewien.Pages.Modul08
+{
+    public class MiniFormModel : PageModel
+    {
+        [BindProperty]
+        public Person MyPerson { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ aspnetcorewien/Pages/Modul08/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcorewien.Pages.Modul08
+{
+    public class Person
+    {
+        [Display(Name="Hey wie ald?")]
+        [Range(18,99,ErrorMessage ="du darfst hier nicht rein")]
+        public int Alter { get; set; }
+        [Required(ErrorMessage ="mach da was rein!")]
+        public String Name { get; set; }
+    }
+}
