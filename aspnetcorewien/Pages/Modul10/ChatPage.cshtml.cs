4b8f824ced85effbc91d96d02b7f3a32ba2effff
Hannes Preishuber <hannesp@ppedv.de>
2019-06-28 15:08:23 +0200

    SignalR Chat

---
+++ aspnetcorewien/Pages/Modul10/ChatPage.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcorewien.Pages.Modul10
+{
+    public class ChatPageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ aspnetcorewien/Pages/Modul10/MyChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcorewien.Pages.Modul10
+{
+    public class MyChatHub:Hub
+    {
+
+        public void Send(string msg)
+        {
+            Clients.All.SendAsync("ETClient", msg);
+        }
+    }
+}
